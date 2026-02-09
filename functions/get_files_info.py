02027f6c92920edd169bb41417c94e12deb999a8
henkster <steve@henkster.com>
2026-02-09 10:51:14 -0600

    Modify getting files to handle more exception possibilities and be more performant in the loop. Still is a bit rough, but it is funcitonal enough.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,17 +1,18 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    if os.path.commonpath([working_dir_abs, target_dir]) != working_dir_abs:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a directory'
+    try: # Moved this up to top because some of the initial OS function calls can raise exceptions/
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        if os.path.commonpath([working_dir_abs, target_dir]) != working_dir_abs:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
 
-    result = f"Result for {'current' if directory == "." else f"'{directory}'"} directory:"
-    try:
-        for item in os.listdir(target_dir):
-            result += f"\n- {item}: file_size: {os.path.getsize(os.path.normpath(os.path.join(target_dir, item)))} bytes, is_dir={os.path.isdir(os.path.normpath(os.path.join(target_dir, item)))}"
-        return result
+        result = f"Result for {'current' if directory == "." else f"'{directory}'"} directory:"
+        files_info = [] # preferable to concatenating a string in each loop since string is immutable and a new one is created each time.
+        for filename in os.listdir(target_dir):
+            files_info.append(f"\n- {filename}: file_size: {os.path.getsize(os.path.join(target_dir, filename))} bytes, is_dir={os.path.isdir(os.path.normpath(os.path.join(target_dir, filename)))}")
+        return "".join(files_info)
     except Exception as e:
         return f"Error: An error occurred reading the directory contents: {e}"
