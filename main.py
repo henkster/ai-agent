6afb6de4ef0f493721c698e83306d0405d7993cf
henkster <steve@henkster.com>
2026-02-15 11:19:19 -0600

    Add recursive calls so LLM see results and acts on each until no more calls are needed.

--- main.py
+++ main.py
@@ -34,37 +34,46 @@ def main():
     #     model="gemini-2.5-flash", contents=messages
     # )
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=config,
-    )
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=config,
+        )
 
-    if response.usage_metadata is None:
-        raise RuntimeError("No response usage metadata, possible API request failure.")
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    function_results = []
-    
-    if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    if not response.function_calls is None:
-        for function_call in response.function_calls:
-            #print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call, verbose)
-            if not function_call_result.parts:
-                raise Exception("No parts") #return Error: ...?
-            if function_call_result.parts[0].function_response is None:
-                raise Exception("Not function response")
-            if function_call_result.parts[0].function_response.response is None:
-                raise Exception("No response")
-            function_results.append(function_call_result.parts[0])
-            if verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
+        if response.usage_metadata is None:
+            raise RuntimeError("No response usage metadata, possible API request failure.")
+
+        function_results = []
+        
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        if not response.function_calls is None:
+            for function_call in response.function_calls:
+                #print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call, verbose)
+                if not function_call_result.parts:
+                    raise Exception("No parts") #return Error: ...?
+                if function_call_result.parts[0].function_response is None:
+                    raise Exception("Not function response")
+                if function_call_result.parts[0].function_response.response is None:
+                    raise Exception("No response")
+                function_results.append(function_call_result.parts[0])
+                if verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_results))
+        else:
+            print(response.text)
+            return
+    print("Something went wrong, no results")
+    exit(code=1)
 
 def get_args():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
